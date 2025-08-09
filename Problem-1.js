dc4462767fb1f6d358c87c7ebef08f3cc1ad031a
Prachurya Ray <prachuryaray@gmail.com>
2025-08-10 00:33:30 +0530

    first problem solved

---
+++ Problem-1.js
@@ -0,0 +1,26 @@
+var a = parseInt(prompt("Enter value of a:"));
+var b = parseInt(prompt("Enter value of b:"));
+const c = prompt("Enter type of operation:");
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+function printing(func) {
+  alert("Result: "+func(a, b));
+}
+
+function errorPrint(){
+    alert("Invalid operation");
+}
+
+function choose() {
+  if (c == "+") printing(add);
+  else if (c == "-") printing(sub);
+  else if (c == "*") printing(mul);
+  else if (c == "/") printing(div);
+  else errorPrint();
+}
+
+choose(c);
